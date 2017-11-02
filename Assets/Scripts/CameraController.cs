c66afe4486aac67b64732a1fde3a9f353bd8c185
ClaySpike <povinellimax@gmail.com>
2017-11-02 17:54:58 -0400

    Added the base Game
    Added the base game that i had been working on for a while

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValues;
+    public float rotateSpeed;
+    public Transform pivot;
+
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+
+	// Use this for initialization
+	void Start () {
+		if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        //X pos of mouse and rotate target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0f, horizontal, 0f);
+
+        //Y pos mouse and rotate pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0f, 0f);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0f, 0f);
+        }
+
+        //Limit up/down camera rotation
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0f, 0f);
+        }
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0f, 0f);
+        }
+
+        //Move camera based on rotation of target and original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0f);
+        transform.position = target.position - (rotation * offset);
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+        }
+        transform.LookAt(target);
+	}
+}
